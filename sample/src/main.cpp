d06e69bb2a3f9122045ab1874d2cbe72b9ac3f44
Gergo Papp <geri.papp17@gmail.com>
2022-06-30 17:11:17 +0200

    Same output as the current master

--- sample/src/main.cpp
+++ sample/src/main.cpp
@@ -3,7 +3,7 @@
 int main(int argc, char** argv) {
 
     PointCloud map, prev_cloud;
-    cv::Mat body_to_body0 = cv::Mat::eye(4, 4, CV_32F);
+    cv::Mat T = cv::Mat::eye(4, 4, CV_32F);
 
     for (int i = 20; i < 50; ++i) {
 
@@ -16,24 +16,26 @@ int main(int argc, char** argv) {
         ifs.close();
 
         if (!prev_cloud.empty()) {
-            cv::Mat T = vanilla_icp(prev_cloud, cur_cloud);
-
-            body_to_body0 = T * body_to_body0;
+            cv::Mat T_new = vanilla_icp(prev_cloud, cur_cloud);
+            T = T_new * T;
 
             const auto& cur_cloud_points = cur_cloud.get_points();
             for (int i = 0; i < cur_cloud.size(); ++i) {
                 cv::Vec4f point { cur_cloud_points.at<float>(i, 0), cur_cloud_points.at<float>(i, 1), cur_cloud_points.at<float>(i, 2), 1.0f };
-                cv::Mat point_transformed  = body_to_body0 * cv::Mat { point };
+                cv::Mat point_transformed  = T * cv::Mat { point };
                 point_transformed /= point_transformed.at<float>(3);
+                point_transformed.pop_back();
+                cv::transpose(point_transformed, point_transformed);
+
                 map.add_point( point_transformed );
             }
+
+            std::ofstream ofs{ "/home/geri/work/c++/Lidar-Odometry/output/map" + std::to_string(i) + ".ply", std::ofstream::out };
+            write_ply(ofs, map, { (rand() + 100) % 256, (rand() + 100) % 256, (rand() + 100) % 256 });
+            ofs.close();
         }
 
         prev_cloud = cur_cloud;
-
-        std::ofstream ofs{ "/home/geri/work/c++/Lidar-Odometry/output/map" + std::to_string(i) + ".ply", std::ofstream::out };
-        write_ply(ofs, map, { (rand() + 100) % 256, (rand() + 100) % 256, (rand() + 100) % 256 });
-        ofs.close();
     }
 
     return 0;
